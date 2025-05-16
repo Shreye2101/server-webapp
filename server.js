37fa1948fc9f2677788f56c87553aeb057c67f2b
shreyash <shreye2101@gmail.com>
2025-05-16 12:12:51 +0530

    mongoDB URL push

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ const commonFeatureRouter = require("./routes/common/feature-routes");
 //create a separate file for this and then import/use that file here
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/shop")
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
 
